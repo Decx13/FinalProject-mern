3cd7f485d8cde28500c27191c70bc156b2640026
amandimaheshavi <amandimaheshavi@gmail.com>
2024-01-25 23:03:28 +0530

    adding upload and manage section for admin

--- mern-client/src/dashboard/ManageFurniture.jsx
+++ mern-client/src/dashboard/ManageFurniture.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Table } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
 const ManageFurniture = () => {
+const [allFurniture, setAllFurniture] = useState([]);
+useEffect( () =>{
+  fetch("http://localhost:5000/all-furniture").then(res => res.json()).then(data => setAllFurniture(data));
+},[])
+
   return (
-    <div>ManageFurniture</div>
+    <div className='px-4 my-12'>
+      <h2 className='mb-8 text-3xl font-bold'>Manage Furniture</h2>
+
+      {/* table for furniture */}
+      <Table className='lg:w-[1180px]'>
+        <Table.Head>
+          <Table.HeadCell>Number</Table.HeadCell>
+          <Table.HeadCell>Furniture Name</Table.HeadCell>
+          <Table.HeadCell>Style Title</Table.HeadCell>
+          <Table.HeadCell>Category</Table.HeadCell>
+          <Table.HeadCell>Prices</Table.HeadCell>
+          <Table.HeadCell>
+            <span>Manage</span>
+          </Table.HeadCell>
+        </Table.Head>
+          {
+            allFurniture.map((furniture, index) => <Table.Body className="divide-y" key={furniture._id}>
+
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {index+1}
+            </Table.Cell>
+            <Table.Cell>{furniture.furnitureName}</Table.Cell>
+            <Table.Cell>{furniture.styleTitle}</Table.Cell>
+            <Table.Cell>{furniture.category}</Table.Cell>
+            <Table.Cell>{25000.00}</Table.Cell>
+
+            <Table.Cell>
+              <Link 
+              className="font-medium text-cyan-600 hover:underline dark:text-cyan-500" 
+              to={`/admin/dashboard/edit-furniture/${furniture._id}`}>
+              <p>
+                Manage
+              </p>
+              </Link>
+            </Table.Cell>
+          </Table.Row>
+            </Table.Body> )
+          }
+       
+      </Table>
+
+    </div>
   )
 }
 
--- mern-client/src/dashboard/UploadFurniture.jsx
+++ mern-client/src/dashboard/UploadFurniture.jsx
@@ -10,7 +10,8 @@ const UploadFurniture = () => {
     "Doors",
     "Windows",
     "Mirrors",
-    "Desks"
+    "Desks",
+    "Sofas"
 
   ]
 
@@ -20,11 +21,41 @@ const UploadFurniture = () => {
     console.log(event.target.value);
     setSelectedFurnitureCategory(event.target.value);
   }
+  // submition handle
+  const handleFurnitureSubmit = (event)  => {
+    event.preventDefault();
+    const form = event.target;
+
+    const furnitureName = form.furnitureName.value;
+    const styleTitle = form.styleTitle.value;
+    const imageUrl = form.imageUrl.value;
+    const category = form.category.value;
+    const furnitureDescription = form.furnitureDescription.value;
+    
+    const furnitureObject = {
+      furnitureName,styleTitle,imageUrl,category,furnitureDescription
+    }
+
+    console.log(furnitureObject);
+    // insert to database
+
+    fetch("http://localhost:5000/upload-furniture",{
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(furnitureObject)
+    }).then(res => res.json()).then(data => {
+      // console.log(data)
+      alert("Furniture Uploaded Successfully!")
+      form.reset();
+    })
+  }
   return (
     <div className='px-4 my-12'>
       <h2 className='mb-8 text-3xl font-bold'>Upload A New Furniture</h2>
 
-      <form className="flex lg:w-[1180px] flex-col flex-wrap gap-4">
+      <form onSubmit={handleFurnitureSubmit} className="flex lg:w-[1180px] flex-col flex-wrap gap-4">
       {/* first Row */}
       <div className='flex gap-8'>
 
@@ -42,11 +73,11 @@ const UploadFurniture = () => {
 
       <div className='lg:w-1/2'>
         <div className="mb-2 block">
-          <Label htmlFor="furnitureStyle"
+          <Label htmlFor="styleTitle"
            value="Furniture Style"/>
         </div>
-        <TextInput id="furnitureStyle"
-         name="furnitureStyle" 
+        <TextInput id="styleTitle"
+         name=" styleTitle" 
          placeholder="Modern Rounded Dark" 
          required 
          type="text"/>
@@ -58,11 +89,11 @@ const UploadFurniture = () => {
     <div className='flex gap-8'>
       <div className='lg:w-1/2'>
         <div className="mb-2 block">
-          <Label htmlFor="imageURL"
+          <Label htmlFor="imageUrl"
            value="Furniture Image URL"/>
         </div>
-        <TextInput id="imageURL"
-         name="imageURL" 
+        <TextInput id="imageUrl"
+         name="imageUrl" 
          placeholder="Furniture Image URL" 
          required 
          type="text"/>
@@ -97,6 +128,7 @@ const UploadFurniture = () => {
         className='w-full'
         required rows={6} />
       </div>
+      <Button type="submit">Upload Furniture</Button>
     </form>
   </div>
   )
