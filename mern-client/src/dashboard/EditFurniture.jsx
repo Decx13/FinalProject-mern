a7b6aa073e58ef153a55928473392faec8291c51
amandimaheshavi <amandimaheshavi@gmail.com>
2024-01-26 21:25:12 +0530

    delete and update book components

--- mern-client/src/dashboard/EditFurniture.jsx
+++ mern-client/src/dashboard/EditFurniture.jsx
@@ -1,8 +1,143 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Checkbox, Label, TextInput, Textarea } from 'flowbite-react';
+import { useLoaderData, useParams } from 'react-router-dom';
 
 const EditFurniture = () => {
+  const {id} = useParams();
+  const {furnitureName,styleTitle,imageUrl,category,furnitureDescription} = useLoaderData();
+
+  const furnitureCategories = [
+    "Chairs",
+    "Tables",
+    "Beds",
+    "Doors",
+    "Windows",
+    "Mirrors",
+    "Desks",
+    "Sofas"
+
+  ]
+
+  const [selectedFurnitureCategory, setSelectedFurnitureCategory] = useState(furnitureCategories[0]);
+
+  const handleChangeSelectedValue = (event) =>{
+    console.log(event.target.value);
+    setSelectedFurnitureCategory(event.target.value);
+  }
+  // submition handle
+  const handleUpdate = (event)  => {
+    event.preventDefault();
+    const form = event.target;
+
+    const furnitureName = form.furnitureName.value;
+    const styleTitle = form.styleTitle.value;
+    const imageUrl = form.imageUrl.value;
+    const category = form.category.value;
+    const furnitureDescription = form.furnitureDescription.value;
+    
+    const furnitureObject = {
+      furnitureName,styleTitle,imageUrl,category,furnitureDescription
+    }
+
+    //console.log(furnitureObject);
+    // update database
+    fetch(`http://localhost:5000/furniture/${id}`,{
+      method: "PATCH",
+    }).then(res => res.json()).then(data => {
+      // console.log(data)
+      alert("Furniture Updated Successfully!")
+      form.reset();
+
+    })
+  }
   return (
-    <div>EditFurniture</div>
+    <div className='px-4 my-12'>
+      <h2 className='mb-8 text-3xl font-bold'>Update  Furniture</h2>
+
+      <form onSubmit={handleUpdate} className="flex lg:w-[1180px] flex-col flex-wrap gap-4">
+      {/* first Row */}
+      <div className='flex gap-8'>
+
+      <div className='lg:w-1/2'>
+        <div className="mb-2 block">
+          <Label htmlFor="furnitureName"
+           value="Furniture Name"/>
+        </div>
+        <TextInput id="furnitureName"
+         name="furnitureName" 
+         placeholder="Teak Black Table" 
+         required 
+         type="text"
+         defaultValue={furnitureName}
+         />
+      </div>
+
+      <div className='lg:w-1/2'>
+        <div className="mb-2 block">
+          <Label htmlFor="styleTitle"
+           value="Furniture Style"/>
+        </div>
+        <TextInput id="styleTitle"
+         name=" styleTitle" 
+         placeholder="Modern Rounded Dark" 
+         required 
+         type="text"
+         defaultValue={styleTitle}
+         />
+      </div>
+
+      </div>
+
+      {/* second row */}
+    <div className='flex gap-8'>
+      <div className='lg:w-1/2'>
+        <div className="mb-2 block">
+          <Label htmlFor="imageUrl"
+           value="Furniture Image URL"/>
+        </div>
+        <TextInput id="imageUrl"
+         name="imageUrl" 
+         placeholder="Furniture Image URL" 
+         required 
+         type="text"
+         defaultValue={imageUrl}/>
+      </div>
+      {/* Category */}
+      <div className='lg:w-1/2'>
+      <div className="mb-2 block">
+          <Label htmlFor="inputState"
+           value="Furniture Category"
+
+          />
+        </div>
+
+          <select id="inputState" name="category" className='w-full rounded' 
+          value={selectedFurnitureCategory} onChange={handleChangeSelectedValue}>
+              {
+                furnitureCategories.map((option) => <option key={option} value={option}> {option} </option>)
+              }
+          </select>
+      </div>
+      
+      </div>
+
+      {/* description */}
+      <div>
+      <div className="mb-2 block">
+          <Label htmlFor="furnitureDescription" 
+          value="Furniture Description" />
+        </div>
+        <Textarea 
+        id="furnitureDescription" 
+        name="furnitureDescription"
+        placeholder="Type description here..." 
+        className='w-full'
+        required rows={6}
+        defaultValue={furnitureDescription}/>
+      </div>
+      <Button type="submit">Update Furniture</Button>
+    </form>
+  </div>
   )
 }
 
--- mern-client/src/dashboard/ManageFurniture.jsx
+++ mern-client/src/dashboard/ManageFurniture.jsx
@@ -8,6 +8,20 @@ useEffect( () =>{
   fetch("http://localhost:5000/all-furniture").then(res => res.json()).then(data => setAllFurniture(data));
 },[])
 
+
+ // delete a book
+  const handleDelete = (id) =>{
+    console.log(id);
+
+    fetch(`http://localhost:5000/furniture/${id}`,{
+      method: "DELETE",
+    }).then(res => res.json()).then(data => {
+    alert ("Furniture Deleted!")
+    //setAllFurniture(data);
+  })
+
+}
+
   return (
     <div className='px-4 my-12'>
       <h2 className='mb-8 text-3xl font-bold'>Manage Furniture</h2>
@@ -38,12 +52,11 @@ useEffect( () =>{
 
             <Table.Cell>
               <Link 
-              className="font-medium text-cyan-600 hover:underline dark:text-cyan-500" 
+              className="font-medium text-cyan-600 hover:underline dark:text-cyan-500 mr-5" 
               to={`/admin/dashboard/edit-furniture/${furniture._id}`}>
-              <p>
                 Manage
-              </p>
               </Link>
+              <button onClick={() => handleDelete(furniture._id)} className='bg-red-600 px-4 py-1 font-semibold text-white rounded-sm hover:bg-sky-600'>Delete</button>
             </Table.Cell>
           </Table.Row>
             </Table.Body> )
